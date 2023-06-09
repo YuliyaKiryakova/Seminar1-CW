bc51ea50d72398f57eeeb43f28ae6aa4a5648415
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-09 13:50:03 +0300

    EX2

---
+++ Ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите прогу, которая на вход принимает два числа и и проверяет, является ли первое число квадратом второго.
+a = 25, b = 5 -> да 
+a = 2, b = 10 -> нет 
+a = 9, b = -3 -> да 
+a = -3 b = 9 -> нет
+*/
+
+Console.Write("Введите целое число a: ");
+string numberA = Console.ReadLine()!;
+Console.Write("Введите целое число b: ");
+string numberB = Console.ReadLine()!;
+int numA = int.Parse(numberA);
+int numB = int.Parse(numberB);
+if (numA == numB * numB)
+{
+    Console.WriteLine($"Квадрат числа {numB} равен {numA}");
+}
+else
+{
+Console.WriteLine($"Квадрат числа {numB} не равен {numA}");
+}
