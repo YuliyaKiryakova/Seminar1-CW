1de269648ee4599b190ee61d252a8bc86429b4e9
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-10 14:16:52 +0300

    квадрат числа

---
+++ Ex0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напищите программу, которая на вход принимает число и выдает его квадрат
+
+System.Console.Write("Ведите целое число: ");
+string number = System.Console.ReadLine()!;
+int num = int.Parse(number);
+
+int square = num * num;
+System.Console.WriteLine($"Квадрат числа {num} равен {square}");
+// System.Console.WriteLine(num * num);
